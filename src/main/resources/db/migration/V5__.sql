5d5aeb4f6ace608d95c6b95f027be67c4c15dd59
Etern-34520 <a_34520@163.com>
2025-07-30 10:29:02 +0800

    [Fix] V5__.sql

--- src/main/resources/db/migration/V5__.sql
+++ src/main/resources/db/migration/V5__.sql
@@ -1,12 +1,12 @@
 INSERT INTO server_setting_items (setting_key, clazz, setting_value)
     SELECT 'grading.multipleChoicesQuestionsCheckingStrategy', 'java.lang.String', '"all_correct"'
     FROM dual
-    WHERE NOT EXISTS (SELECT * FROM server_setting_items WHERE setting_key = 'grading.multipleChoicesQuestionsCheckingStrategy');
+    WHERE NOT EXISTS (SELECT 1 FROM server_setting_items WHERE setting_key = 'grading.multipleChoicesQuestionsCheckingStrategy');
 
 INSERT INTO server_setting_items (setting_key, clazz, setting_value)
     SELECT 'grading.enableLosePoints', 'java.lang.Boolean', 'false'
     FROM dual
-    WHERE NOT EXISTS (SELECT * FROM server_setting_items WHERE setting_key = 'grading.enableLosePoints');
+    WHERE NOT EXISTS (SELECT 1 FROM server_setting_items WHERE setting_key = 'grading.enableLosePoints');
 
 UPDATE server_setting_items
     SET setting_key = 'generating.samplingStrategy'
